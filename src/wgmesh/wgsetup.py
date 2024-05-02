76f80352d7875b623a530c40b26e2f4bf031043d
Joshua Schmidlkofer <joshua@joshuainnovates.us>
2024-05-01 21:02:16 -0700

    Update: deploy template contol updates.

--- src/wgmesh/wgsetup.py
+++ src/wgmesh/wgsetup.py
@@ -21,6 +21,8 @@ from .lib import LoggerConfig, filediff
 from .version import VERSION
 from .crypto import *
 from .hostlib import get_local_addresses_with_interfaces
+from .templates import bird_private, ns_private, ns_tester, mesh_start, wireguard_conf
+from .templates import render
 
 app = t.Typer()
 
@@ -288,13 +290,120 @@ def sync(locus:           Annotated[str, t.Argument(help='short/familiar name, s
             cf.write(new_config)
     return 0
 
+def deployfile(locus: str, deploy_path:str) ->Munch:
+    ''' common filename configuration '''
+    retval = {
+        'ns_private': os.path.join(deploy_path, 'usr/local/sbin/ns-private'),
+        'ns_tester': os.path.join(deploy_path, 'usr/local/sbin/ns-tester'),
+        'mesh_wg_restart': os.path.join(deploy_path, 'usr/local/sbin/mesh_wg_restart'),
+        'meth_ns_init': os.path.join(deploy_path, 'usr/local/sbin/mesh_wg_init'),
+        'bird_private': os.path.join(deploy_path, 'etc/bird/bird_private'),
+        'bird_conf_d': os.path.join(deploy_path, 'etc/bird/bird_private_local.d'),
+    }
+    return munchify(retval)
+
 @app.command()
-def deploy(*args, **kwargs):
+def deploy(locus:           Annotated[str, t.Argument(help='short/familiar name, short hand for this mesh')],
+           config_path:     Annotated[str, t.Argument(envvar="WGM_CONFIG")] = '/etc/wireguard',
+           deploy_path:     Annotated[str, t.Option(help='base for install') = '/',
+           dryrun:          Annotated[bool, t.Option(help='do not write anything')] = False,
+           debug:           Annotated[bool, t.Option(help='debug logging')] = False,
+           trace:           Annotated[bool, t.Option(help='trace logging')] = False):
     ''' deploy local wgmesh configuration and scripts '''
     LoggerConfig(debug, trace)
-    print(f'{args} / {kwargs}')
 
-    return 0
+    filenames = hostfile(locus, config_path)
+    with open(filenames.cfg_file, 'r', encoding='utf-8') as cf:
+        ep = Endpoint.load_endpoint_config(cf)
+        pass
+    ep.open_keys()
+
+    with open(filenames.sync_file, 'r') as sync_file:
+        sync_file = munchify(yaml.load(sync_payload, sync_file))
+
+    portbase = sync_file.portbase
+    site = sync_file.site
+    tunnel_network = netaddr.IPNetwork(sync_file.remote)
+    tunnel_net_base = str(tunnel_network.network).split('::')[0]
+    mykey = open(ep.secret_key_file, 'r').read().strip()
+    template_args = munchify({})
+    template_args.interface_outbound = ep.public_iface
+    template_args.interface_trust = ep.trust_iface
+    template_args.wireguard_interfaces = {}
+    template_args.cmds = { 'binfping': ep.cmdfping }
+    template_args.locus = ep.locus
+    template_args.myhost = ep.hostname
+    template_args.local_asn = ep.asn
+    template_args.octet = sync_file.octet
+    template_args.tunnel_remote = sync_file.remote
+    deploy_files = deployfile(locus, deploy_path)
+    for host, values in sync_file.hosts.items():
+        index = values.localport - sync_file.portbase
+        remotes = ''
+        if len(values.remote):
+            remotes = ','.join( [ f"{str(x)}:{values.remoteport}" for x in values.remote.split(',') ] )
+            portpoints = [ sync_file.octet ]
+            portpoints.append( index )
+            netbits = ''.join([ '{:02X}'.format(a) for a in sorted(portpoints, reverse = True) ])
+            local_endpoint_addr = f'{tunnel_net_base}:{netbits}::{deploy_message["octet"]}/64'
+            remote_endpoint_addr = f'{tunnel_net_base}:{netbits}::{index}'
+            listen_address = template_args['interface_trust_ip'].split('/')[0]
+
+            fulfill = {
+                'myhost':           ep.hostname,
+                'Hostname':         host,
+                'interface_outbound': template_args.interface_outbound,
+                'interface_public': template_args.interface_public,
+                'interface_trust':template_args.interface_trust,
+                'listen_address': listen_address,
+                'local_port': values['localport'],
+                'octet': deploy_message['octet'],
+                'private_key': mykey,
+                'public_key': values['key'],
+                'remote_address': remotes,
+                'tunnel_addresses': local_endpoint_addr,
+            }
+            template_args.ports.append( values['localport'] )
+            template_args.wireguard_interfaces[f'wg{index}'] = [ remote_endpoint_addr, values['asn'] ]
+            template_args.local_endpoint_addr = local_endpoint_addr
+            wgconf = render(wireguard_conf, fulfill)
+            if dry_run:
+                logger.info(f'Dry-run Mode.')
+                print(wgconf)
+            else:
+                check_update_file(wgconf, os.path.join(deploy_path, f'/etc/wireguard/wg{index}.conf'))
+                pass
+            continue
+
+        nssysvinit = render(ns_private, template_args)
+        tssysvinit = render(ns_tester, template_args)
+        meshstart = render(mesh_start, template_args)
+        bird_priv = render(bird_private,template_args)
+        check_update_file(nssysvinit, deploy_file.ns_private)
+        check_update_file(tssysvinit, deploy_file.ns_tester)
+        check_update_file(meshstart, deploy_file.mesh_wg_restart)
+        check_update_file(bird_priv, deploy_file.bird_private)
+        for x in (deploy_file.ns_private,
+                  deploy_file.ns_tester,
+                  deploy_file.mesh_wg_restart):
+            os.chmod(x, 0o750)
+            continue
+        os.chmod(deploy_file.bird_private, 0o640)
+        buser = pwd.getpwnam('bird').pw_uid
+        bgroup = pwd.getpwnam('bird').pw_gid
+        if not os.path.exists(deploy_file.bird_conf_d):
+            os.makedirs(deploy_file.bird_conf_d)
+            try:
+                os.chown(deploy_file.bird_conf_d, buser, bgroup)
+            except PermissionError:
+                logger.warning(f'Failed to set ownership of {deploy_file.bird_conf_d}')
+                pass
+            pass
+        try:
+            os.chown(deploy_file.bird_private, buser, bgroup)
+        except PermissionError:
+            logger.warning(f'Failed to set ownership of /etc/bird/bird_private.conf')
+        return 0
 
 if __name__ == "__main__":
     app()
